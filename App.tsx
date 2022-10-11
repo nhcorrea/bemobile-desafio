db795339fc8c9a3348f2db7cf292145b0a600261
Nathã Hernandez <nathahcorrea@gmail.com>
2022-10-11 03:56:05 -0300

    feat: simple base project created

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { THEME } from "./src/theme";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context"
+
+import Loading from "./src/components/Loading";
+import Home from "./src/screen/Home";
+import ContextProvider from "./src/context/context";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from "@expo-google-fonts/roboto";
+
+export default function App() { 
+
+  const [ fontsLoaded ] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium
+  });
+
+  return (
+      <SafeAreaView>
+        <ContextProvider>
+          {fontsLoaded ? <Home /> : <Loading />}
+          <StatusBar style='light' backgroundColor={THEME.COLORS.BLACK} />
+        </ContextProvider>
+      </SafeAreaView>
+  );
+}
