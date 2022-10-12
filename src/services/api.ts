26541a85fa5cad4835d866587065f40e66dcaee1
Nathã Hernandez <nathahcorrea@gmail.com>
2022-10-12 00:33:39 -0300

    feat: configuration axios comments

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 /*
-## REACT NATIVE NÃO ENTENDE "LOCAL_HOST" ENTÃO UTILIZAMOS O IP
+## REACT NATIVE NÃO ENTENDE "LOCAL_HOST" ENTÃO UTILIZAMOS O IP LOCAL
 ##
 ## DADOS DA CONFIGURAÇÃO NO PACKAGE.JSON - SCRIPTS
 ##
@@ -10,13 +10,12 @@ import axios from "axios";
 ## EM LOCAL_IP COLOCAMOS O IP UTILIZADO EM --host E EM PORT A PORTA DISPONIBILIZADA EM --port 4444
 ##
 ## EXEMPLO:
-##
 ## LOCAL_IP = "192.168.100.7"
 ## PORT = "4444"
 */
 
 const LOCAL_IP = "192.168.100.7";
-const PORT = "4444"
+const PORT = "4444";
 
 export const api = axios.create({
     baseURL: `http://${LOCAL_IP}:${PORT}`
