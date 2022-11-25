3f3433f8ebab87ebd8d2cc15f20c87f09c1ccb46
Nathã Hernandez <nathasixth6@gmail.com>
2022-11-25 10:45:45 -0300

    feat: added styled theme type

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,26 @@
+import "styled-components";
+
+declare module "styled-components" {
+	export interface DefaultTheme {
+			COLORS: {
+				BLUE_PRIMARY: string;
+				BLACK: string;
+				GRAY_10: string;
+				GRAY_20: string;
+				GRAY_00: string;
+				WHITE: string;
+			},
+			FONT_FAMILY: {
+				MEDIUM: string;
+				REGULAR: string;
+			},
+			FONT_SIZE:{
+				HEADING_1: string;
+				HEADING_2_AND_3: string;
+			},
+			BOX_SHADOW: {
+				SHADOW_1: string;
+				SHADOW_2: string;
+			}
+		}
+}
