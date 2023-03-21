6a0544f375a0a2dd9f719dec41ac4bf3807c3e5f
Nathã Hernandez <nathahcorrea@gmail.com>
2023-03-21 13:33:42 -0300

    refactor: add environment variables to axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,6 @@
-import axios from "axios";
-
-const LOCAL_IP = "192.168.3.7";
-const PORT = "4444";
+import axios from "axios"
+import { LOCAL_HOST, PORT } from "@env"
 
 export const api = axios.create({
-	baseURL: `http://${LOCAL_IP}:${PORT}`
-});
+	baseURL: `http://${LOCAL_HOST}:${PORT}`
+})
